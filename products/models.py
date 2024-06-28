5f93af9c1ee783640be491d7e66c5d6839b2a9ef
Luis Martinez <lcmartinezl05@gmail.com>
2024-06-27 19:59:16 -0500

    Add products app

---
+++ products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.TextField(max_length=200, verbose_name="nombre")
+    description = models.TextField(max_length=300, verbose_name="descripción")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
+    available = models.BooleanField(default=True, verbose_name="disponible")
+    photo = models.ImageField(upload_to="logos", null=True, blank=True, verbose_name="foto")
+
+    def __srt__(self):
+        return self.name
