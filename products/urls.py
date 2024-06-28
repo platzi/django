465ac12fa7550cbef49d9a4082fc00961533c58f
Luis Martinez <lcmartinezl05@gmail.com>
2024-06-28 18:04:05 -0500

    Challenge resolved

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductFormView
+from .views import ProductFormView, ProductListView
 
 
 urlpatterns = [
+    path('', ProductListView.as_view(), name='list_product'),
     path('agregar/', ProductFormView.as_view(), name="add_product"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
+
+from products.models import Product
 from .forms import ProductForm
 
 class ProductFormView(generic.FormView):
@@ -11,4 +13,8 @@ class ProductFormView(generic.FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
-    
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list_product.html'
+    context_object_name = 'products'
