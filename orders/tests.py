5eb1ecc2a38372f80c0a5fb6938760648a2b657d
Luis Martinez <lcmartinezl05@gmail.com>
2024-07-03 20:45:09 -0500

    Add tests

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class MyOrderViewTests(TestCase):
+
+    def test_no_logged_user_should_redirect(self):
+        url = reverse('my_order')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/usuarios/login/?next=/pedidos/mi-orden')
+
+    def test_logged_user_should_redirect(self):
+        url = reverse('my_order')
+        user = get_user_model().objects.create(username="test")
+        self.client.force_login(user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Product
+
+class ProductListViewTests(TestCase):
+
+    def test_should_return_200(self):
+        url = reverse('list_product')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['products'].count(), 0)
+
+    def test_should_return_200_with_products(self):
+        url = reverse('list_product')
+        Product.objects.create(
+            name="test",
+            description="test description",
+            price="5",
+            available=True
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['products'].count(), 1)
